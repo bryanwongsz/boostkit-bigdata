5eae5261818d83b7ebe5e41b14baf5624d207eba
Jmist <fishmist@163.com>
2022-06-20 15:50:23 +0800

    update license info

--- omnioperator/omniop-spark-extension/cpp/test/shuffle/shuffle_test.cpp
+++ omnioperator/omniop-spark-extension/cpp/test/shuffle/shuffle_test.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+ * Copyright (C) 2020-2022. Huawei Technologies Co., Ltd. All rights reserved.
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information

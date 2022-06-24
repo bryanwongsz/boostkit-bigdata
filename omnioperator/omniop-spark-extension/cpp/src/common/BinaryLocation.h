1cccf10451d7378604d60913eb9037d42bfd853b
Jmist <fishmist@163.com>
2022-06-24 16:20:00 +0800

    modify sql file and OmniExpressionAdaptorSuite

--- omnioperator/omniop-spark-extension/cpp/src/common/BinaryLocation.h
+++ omnioperator/omniop-spark-extension/cpp/src/common/BinaryLocation.h
@@ -21,7 +21,8 @@
 class VCLocation {
 public:
     VCLocation(uint64_t vc_addr, uint32_t vc_len, bool isnull)
-            : vc_len(vc_len), vc_addr(vc_addr), is_null(isnull) {
+            : vc_len(vc_len), vc_addr(vc_addr), is_null(isnull)
+            {
             }
     ~VCLocation() {
     }
